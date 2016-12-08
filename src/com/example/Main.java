f85603718b2d286542d9eed77441c33e519d8f57
BLizak <bartlizak@gmail.com>
2016-12-08 21:48:23 +0100

    Taki tam commit.

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,7 +3,7 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	    int i = 9;
+	    int i = 99;
 	    System.out.println("Hello world. No elo.");
 	    System.out.println("Commit change");
     }
